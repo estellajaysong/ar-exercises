0066b9b18fff725bf6b3949be908a7f669c05cf5
Estella Song <estellajaysong@gmail.com>
2019-04-16 23:41:39 +0000

    Completed Exercise 5: Calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,12 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+allStoresRev = Store.all.sum(:annual_revenue)
+puts allStoresRev
+
+allStoresAvg = Store.all.average(:annual_revenue)
+puts allStoresAvg
+
+#Output the number of stores that are generating $1M or more in annual sales.
+numStores = Store.where("annual_revenue > ?", 1000000).count
+puts numStores
